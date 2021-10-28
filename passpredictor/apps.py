91dcab61fa94994b210696eef423cdf1d55462c2
jordan.adopo <jordan.adopo@gmail.com>
2021-10-28 22:10:08 +0000

    Initial commit

--- passpredictor/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.conf import settings
-import os
-import pickle
-
-
-class PasspredictorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'passpredictor'
